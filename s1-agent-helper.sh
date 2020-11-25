3b75a70c3a937518610142790db9e244dec37caa
s1-howie <howieh@sentinelone.com>
2020-11-25 08:14:10 -0500

    Added colorized output for errors, info and success messages

--- s1-agent-helper.sh
+++ s1-agent-helper.sh
@@ -23,26 +23,31 @@ AGENT_INSTALL_SYNTAX=''
 AGENT_FILE_NAME=''
 AGENT_DOWNLOAD_LINK=''
 
+Color_Off='\033[0m'       # Text Resets
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+
+
 # Check if running as root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "This script must be run as root.  Please retry with 'sudo'."
+    printf "\n${Red}ERROR:  This script must be run as root.  Please retry with 'sudo'.${Color_Off}\n"
     exit 1;
 fi
 
 # Check if correct # of arguments are passed.
 if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 S1_CONSOLE_PREFIX API_KEY SITE_TOKEN VERSION_STATUS" >&2
-  exit 1
+    printf "\n${Red}ERROR:  Incorrect number of arguments were passed.${Color_Off}\n"
+    echo "Usage: $0 S1_CONSOLE_PREFIX API_KEY SITE_TOKEN VERSION_STATUS" >&2
+    echo ""
+    exit 1
 fi
 
 # Check if curl is installed.
 function curl_check () {
     if ! [[ -x "$(which curl)" ]]; then
-    echo ""
-        echo "################################################################################"
-        echo "# INSTALLING CURL UTILITY IN ORDER TO INTERACT WITH S1 API"
-        echo "################################################################################"
-        echo ""
+        printf "\n${Yellow}INFO:  Installing curl utility in order to interact with S1 API... ${Color_Off}\n"
         if [[ $1 = 'apt' ]]; then
             sudo apt-get update && sudo apt-get install -y curl
         elif [[ $1 = 'yum' ]]; then
@@ -53,42 +58,41 @@ function curl_check () {
         elif [[ $1 = 'dnf' ]]; then
             sudo dnf install -y curl
         else
-            echo "unsupported file extension!" # Note.. might need to handle dnf in the future
+            printf "\n${Red}ERROR:  Unsupported file extension.${Color_Off}\n"
         fi
     else
-        echo "curl is already installed... :)"
+        printf "${Yellow}INFO:  curl is already installed.${Color_Off}\n"
     fi
 }
 
 # Check if the SITE_TOKEN is in the right format
 if ! [[ ${#SITE_TOKEN} -gt 100 ]]; then
-    echo "Invalid format for SITE_TOKEN: $SITE_TOKEN"
+    printf "\n${Red}ERROR:  Invalid format for SITE_TOKEN: $SITE_TOKEN ${Color_Off}\n"
     echo "Site Tokens are generally more than 100 characters long and are ASCII encoded."
+    echo ""
     exit 1
 fi
 
 # Check if the API_KEY is in the right format
 if ! [[ ${#API_KEY} -eq 80 ]]; then
-    echo "Invalid format for API_KEY: $API_KEY"
+    printf "\n${Red}ERROR:  Invalid format for API_KEY: $API_KEY ${Color_Off}\n"
     echo "API Keys are generally 80 characters long and are alphanumeric."
+    echo ""
     exit 1
 fi
 
 # Check if the VERSION_STATUS is in the right format
 if [[ ${VERSION_STATUS} != *"GA"* && "$VERSION_STATUS" != *"EA"* ]]; then
-    echo "Invalid format for VERSION_STATUS: $VERSION_STATUS"
+    printf "\n${Red}ERROR:  Invalid format for VERSION_STATUS: $VERSION_STATUS ${Color_Off}\n"
     echo "The value of VERSION_STATUS must contain either 'EA' or 'GA'"
+    echo ""
     exit 1
 fi
 
 
 function jq_check () {
     if ! [[ -x "$(which jq)" ]]; then
-        echo ""
-        echo "################################################################################"
-        echo "# INSTALLING JQ UTILITY IN ORDER TO PARSE JSON RESPONSES FROM API"
-        echo "################################################################################"
-        echo ""
+        printf "\n${Yellow}INFO:  Installing jq utility in order to parse json responses from api... ${Color_Off}\n"
         if [[ $1 = 'apt' ]]; then
             sudo apt-get update && sudo apt-get install -y jq
         elif [[ $1 = 'yum' ]]; then
@@ -99,10 +103,10 @@ function jq_check () {
         elif [[ $1 = 'dnf' ]]; then
             sudo dnf install -y jq
         else
-            echo "unsupported file extension!" # Note.. might need to handle dnf in the future
+            printf "\n${Red}ERROR:  unsupported file extension: $1 ${Color_Off}\n"
         fi 
     else
-        echo "jq is already installed... :)"
+        printf "${Yellow}INFO:  jq is already installed.${Color_Off}\n"
     fi
 }
 
@@ -137,19 +141,27 @@ elif (cat /etc/*release |grep 'ID="fedora"' || cat /etc/*release |grep 'ID=fedor
     PACKAGE_MANAGER='dnf'
     AGENT_INSTALL_SYNTAX='rpm -i --nodigest'
 else
-    echo "Unknown Release ID"
+    printf "\n${Red}ERROR:  Unknown Release ID: $1 ${Color_Off}\n"
     cat /etc/*release
+    echo ""
 fi
 
 curl_check $PACKAGE_MANAGER
 jq_check $PACKAGE_MANAGER
 sudo curl -H "Accept: application/json" -H "Authorization: ApiToken $API_KEY" "$S1_MGMT_URL$API_ENDPOINT?countOnly=false&packageTypes=Agent&osTypes=linux&sortBy=createdAt&limit=20&fileExtension=$FILE_EXTENSION&sortOrder=desc" > response.txt
 get_latest_version
+printf "\n${Yellow}INFO:  Downloading $AGENT_FILE_NAME ${Color_Off}\n"
 sudo curl -H "Authorization: ApiToken $API_KEY" $AGENT_DOWNLOAD_LINK -o /tmp/$AGENT_FILE_NAME
+printf "\n${Yellow}INFO:  Installing S1 Agent: $(echo "sudo $AGENT_INSTALL_SYNTAX /tmp/$AGENT_FILE_NAME") ${Color_Off}\n"
 sudo $AGENT_INSTALL_SYNTAX /tmp/$AGENT_FILE_NAME
+printf "\n${Yellow}INFO:  Setting Site Token... ${Color_Off}\n"
 sudo /opt/sentinelone/bin/sentinelctl management token set $SITE_TOKEN
+printf "\n${Yellow}INFO:  Starting Agent... ${Color_Off}\n"
 sudo /opt/sentinelone/bin/sentinelctl control start
 
 #clean up files..
+printf "\n${Yellow}INFO:  Cleaning up files... ${Color_Off}\n"
 rm -f response.txt
 rm -f /tmp/$AGENT_FILE_NAME
+
+printf "\n${Green}SUCCESS:  Finished installing SentinelOne Agent. ${Color_Off}\n\n"
