72f50c9107e93dbb2edc0607ea4ba9cf7f64b972
s1-howie <howieh@sentinelone.com>
2021-12-28 15:10:00 -0500

    updated ps1 to install msi instead of exe

--- s1-agent-helper.ps1
+++ s1-agent-helper.ps1
@@ -12,14 +12,14 @@ param(
     [string]$auto_reboot
     )
 
-write-output "Console:          $s1_console_prefix"
-write-output "API Key:          $api_key"
-write-output "Site Token:       $site_token"
-write-output "Version Status:   $version_status"
+write-output "Console:             $s1_console_prefix"
+#write-output "API Key:             $api_key"
+#write-output "Site Token:          $site_token"
+write-output "Version Status:      $version_status"
 
 
 $s1_mgmt_url = "https://$s1_console_prefix.sentinelone.net"
-Write-Output "mgmt url:         $s1_mgmt_url"
+Write-Output "mgmt url:            $s1_mgmt_url"
 $api_endpoint = "/web/api/v2.1/update/agent/packages"
 $agent_file_name = ""
 $agent_download_link = ""
@@ -40,13 +40,18 @@ if ($version_status -ne "GA" -and $version_status -ne "EA") {
     exit 1
 }
 
-Write-Output "all good"
+#Write-Output "all good"
 
 $uri = $s1_mgmt_url + $api_endpoint
-Write-Output $uri
+Write-Output "Using API Endpoint:  $uri"
 
 # Configure HTTP header for API Calls
 $apiHeaders = @{"Authorization"="APIToken $api_key"}
+
+# Check if we need a 32 or 64bit package
+$osArch = "64 bit"
+if($env:PROCESSOR_ARCHITECTURE -eq "x86"){$osArch = "32 bit"}
+
 # The body contains parameters to search for packages with .exe file extensions.. ordering by latest version.
 $body = @{
     "limit"=10
@@ -54,21 +59,20 @@ $body = @{
     "osTypes"="windows"
     "countOnly"="false"
     "sortBy"="createdAt"
-    "fileExtension"=".exe"
+    "fileExtension"=".msi"
     "sortOrder"="desc"
+    "osArches"=$osArch
     }
 # Query the S1 API
 $response = Invoke-RestMethod -Uri $uri -Headers $apiHeaders -Method Get -ContentType "application/json" -Body $body
 # Store the response data as a list of objects
 $packages = $response.data
 
-# Check if we need a 32 or 64bit package
-$osArch = "64 bit"
-if($env:PROCESSOR_ARCHITECTURE -eq "x86"){$osArch = "32 bit"}
+
 
 #Note: "$version_status*"" will match either GA or GA-SP1, GA-SP2, etc
 foreach ($package in $packages) {
-    if ($package.status -like "$version_status*" -and $package.osArch -eq $osArch) {
+    if ($package.status -like "$version_status*") {
         $agent_download_link = $package.link
         $agent_file_name = $package.fileName
         break
@@ -82,12 +86,12 @@ Write-Output "Agent Download Link: $agent_download_link"
 $wc = New-Object System.Net.WebClient
 $wc.Headers['Authorization'] = "APIToken $api_key"
 $wc.DownloadFile($agent_download_link, "$env:TEMP\$agent_file_name")
+
 if($auto_reboot -eq "True") {
-    # Execute the package with the quiet option
-    & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /reboot
+    # Execute the package with the quiet option and force restart
+    msiexec /i "$env:TEMP\$agent_file_name" /q SITE_TOKEN=$site_token /forcerestart
 }
 else {
-    # Execute the package with the quiet option
-    & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /NORESTART
+    # Execute the package with the quiet option and do NOT restart
+    msiexec /i "$env:TEMP\$agent_file_name" /q SITE_TOKEN=$site_token /norestart
 }
-
